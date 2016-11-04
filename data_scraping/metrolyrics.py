f18a3c5041c65e9e449c9cda49e08bbf32824486
Chaitanya Malaviya <chaitanya@ChaitancbookAir.wv.cc.cmu.edu>
2016-11-03 20:09:12 -0400

    word to phoneme framework

--- data_scraping/metrolyrics.py
+++ data_scraping/metrolyrics.py
@@ -13,6 +13,7 @@ PAGE_URLS = [BASE_URL + '/artists-' + ch + '-' for ch in string.ascii_lowercase]
 artist_links = []
 song_links = []
 
+
 def get_artist_links(page_url):
 
 	html = urlopen(page_url).read()
@@ -41,6 +42,22 @@ def get_song_links(name, genre, link, checkSub):
 
 	return links
 
+def get_song_lyrics(link):
+	lyrics = []
+	try:	
+		html = urlopen(link).read()
+		soup = BeautifulSoup(html, "html.parser")
+		html_lyrics = soup.find('div', id="lyrics-body-text").find_all('p')
+
+		for v in html_lyrics:
+			lyrics += [str(v).replace("<br>"," ").replace("</br>"," ").replace("<p class=\"verse\">","").replace("</p>","").strip()]
+
+	except:
+		return "None"
+
+	return " \n".join(lyrics)
+
+
 # for url in PAGE_URLS:
 # 	html = urlopen(url + "1.html").read()
 # 	soup = BeautifulSoup(html, "html.parser")
@@ -57,15 +74,31 @@ def get_song_links(name, genre, link, checkSub):
 # with open("metrolyrics_artist_urls.txt", "w") as f:
 # 	f.write("\n".join(artist_links))
 
-with open("metrolyrics_artist_urls.txt") as f:
-	artist_links = f.readlines()
+# with open("metrolyrics_artist_urls.txt") as f:
+# 	artist_links = f.readlines()
 
-for link in artist_links:
-	artist = make_tuple(link.strip())
-	song_links += get_song_links(artist[0], artist[1], artist[2], True)
+# for link in artist_links:
+# 	artist = make_tuple(link.strip())
+# 	song_links += get_song_links(artist[0], artist[1], artist[2], True)
 
-song_links = [str(entry) for entry in song_links]
+# song_links = [str(entry) for entry in song_links]
 
-print "Writing song links file..."
-with open("metrolyrics_song_urls.txt", "w") as f:
-	f.write("\n".join(song_links))
+# print "Writing song links file..."
+# with open("metrolyrics_song_urls.txt", "w") as f:
+# 	f.write("\n".join(song_links))
+
+with open("metrolyrics_song_urls.txt") as f:
+	song_links = f.readlines()
+
+i = 696678
+for link in song_links[710159:]:
+	song = make_tuple(link.strip())
+	song = list(song)
+	song.insert(3, song[2].split('/')[3].split('-lyrics')[0].replace("-"," "))
+	song = tuple(song)
+	lyrics = get_song_lyrics(song[2])
+	if (lyrics!="None"):
+		with open("songs/song" + str(i) + ".txt","w") as f:
+			f.write(str(song) + "\n")
+			f.write(lyrics)
+		i += 1
